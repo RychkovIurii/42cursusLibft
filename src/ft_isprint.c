11b511bd98c2673fccd8ba586fe962d5ee9c3dd1
Iurii Rychkov <irychkov@student.hive.fi>
2024-04-16 18:32:12 +0300

    int	ft_isprint(int argument)

---
+++ src/ft_isprint.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_isprint.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: irychkov <irychkov@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/16 17:45:39 by irychkov          #+#    #+#             */
+/*   Updated: 2024/04/16 18:31:49 by irychkov         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_isprint(int argument)
+{
+	if (argument >= 32 && argument <= 126)
+		return (1);
+	else
+		return (0);
+}
+/* 
+#include <stdio.h>
+#include <ctype.h>
+int main (void)
+{
+	char s = '\t';
+	int orig, mine;
+	orig = isprint(s);
+	mine = ft_isprint(s);
+	printf("%d is original\n", orig);
+	printf("%d is mine", mine);
+} */
