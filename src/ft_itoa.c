99c16d488ba4477d675489c084b79182cbff2b77
Iurii Rychkov <irychkov@student.hive.fi>
2024-04-21 16:46:04 +0300

    ft_itoa

---
+++ src/ft_itoa.c
@@ -0,0 +1,82 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_itoa.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: irychkov <irychkov@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/21 15:35:21 by irychkov          #+#    #+#             */
+/*   Updated: 2024/04/21 16:45:15 by irychkov         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+static int	ft_digits_len(int n)
+{
+	int	i;
+
+	i = 0;
+	if (n == -2147483648)
+		return (11);
+	else if (n < 0)
+	{
+		i++;
+		n = -n;
+	}
+	while (n > 0)
+	{
+		n = n / 10;
+		i++;
+	}
+	//printf("%d\n", i);
+	return (i);
+}
+
+char	*ft_itoa(int n)
+{
+	int		i;
+	int		len;
+	char	*result;
+
+	i = 0;
+	len = ft_digits_len(n);
+	result = (char *)malloc(sizeof(char) * (len + 1));
+	if (!result)
+		return (NULL);
+	if (n == -2147483648)
+	{
+		ft_strlcpy(result, "-2147483648", 12);
+		return (result);
+	}
+	else if (n < 0)
+	{
+		result[i++] = '-';
+		n = -n;
+	}
+	while (len > i)
+	{
+		len--;
+		result[len] = n % 10 + '0';
+		n = n / 10;
+	}
+	return (result);
+}
+
+int	main(void)
+{
+	ft_digits_len(-11);
+	printf("%s\n", ft_itoa(-11));
+	ft_digits_len(-4443);
+	printf("%s\n", ft_itoa(-4443));
+	ft_digits_len(-1);
+	printf("%s\n", ft_itoa(-1));
+	ft_digits_len(0);
+	printf("%s\n", ft_itoa(0));
+	ft_digits_len(101);
+	printf("%s\n", ft_itoa(101));
+	ft_digits_len(-2147483648);
+	printf("%s\n", ft_itoa(-2147483648));
+
+	return (0);
+}
