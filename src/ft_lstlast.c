c99af35c919db2e9ce87e175d6f3b4eda71dc657
Iurii Rychkov <irychkov@student.hive.fi>
2024-04-23 17:55:10 +0300

    ft_lstlast

--- src/ft_lstlast.c
+++ src/ft_lstlast.c
@@ -6,13 +6,18 @@
 /*   By: irychkov <irychkov@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/23 11:06:23 by irychkov          #+#    #+#             */
-/*   Updated: 2024/04/23 11:07:11 by irychkov         ###   ########.fr       */
+/*   Updated: 2024/04/23 16:50:19 by irychkov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-t_list *ft_lstlast(t_list *lst)
+t_list	*ft_lstlast(t_list *lst)
 {
+	t_list	*ptr;
 
+	ptr = lst;
+	while (ptr != NULL && ptr->next != NULL)
+		ptr = ptr->next;
+	return (ptr);
 }
