847d3aa840dc12eec0a9c6d573f55c7faf257ebd
Iurii Rychkov <irychkov@student.hive.fi>
2024-04-23 13:36:53 +0300

    ft_lstsize

--- src/ft_lstsize.c
+++ src/ft_lstsize.c
@@ -6,20 +6,21 @@
 /*   By: irychkov <irychkov@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/23 11:07:30 by irychkov          #+#    #+#             */
-/*   Updated: 2024/04/23 11:13:27 by irychkov         ###   ########.fr       */
+/*   Updated: 2024/04/23 13:36:29 by irychkov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int ft_lstsize(t_list *lst)
+int	ft_lstsize(t_list *lst)
 {
 	int		i;
 	t_list	*ptr;
 
 	ptr = lst;
 	i = 0;
-	while (ptr!=NULL) {
+	while (ptr != NULL)
+	{
 		i++;
 		ptr = ptr->next;
 	}
