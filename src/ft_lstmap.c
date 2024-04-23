35e1a8e6ba556816166230733de460757bc3f5e7
Rychkov Iurii <rychkoviurii@hotmail.com>
2024-04-23 23:16:24 +0300

    ft_lstmap

--- src/ft_lstmap.c
+++ src/ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: irychkov <irychkov@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/23 14:21:17 by irychkov          #+#    #+#             */
-/*   Updated: 2024/04/23 22:18:31 by irychkov         ###   ########.fr       */
+/*   Updated: 2024/04/23 23:08:45 by irychkov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,16 +16,20 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
 	t_list	*newlist;
 	t_list	*temp;
+	void	*handle_f;
 
 	if (!lst || !f || !del)
 		return (NULL);
 	newlist = NULL;
 	while (lst != NULL)
 	{
-		temp = ft_lstnew(f(lst->content));
+		handle_f = f(lst->content);
+		temp = ft_lstnew(handle_f);
 		if (!temp)
 		{
 			ft_lstclear(&newlist, del);
+			if (handle_f)
+				free(handle_f);
 			return (NULL);
 		}
 		ft_lstadd_back(&newlist, temp);
