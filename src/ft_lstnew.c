c251bb4a4c37c0d0f63684f632dc819161f9b5d0
Iurii Rychkov <irychkov@student.hive.fi>
2024-04-25 13:18:33 +0300

    ft_lstnew

--- src/ft_lstnew.c
+++ src/ft_lstnew.c
@@ -6,7 +6,7 @@
 /*   By: irychkov <irychkov@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/23 00:29:42 by irychkov          #+#    #+#             */
-/*   Updated: 2024/04/23 16:44:26 by irychkov         ###   ########.fr       */
+/*   Updated: 2024/04/25 13:18:16 by irychkov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,3 +23,50 @@ t_list	*ft_lstnew(void *content)
 	ptr->next = NULL;
 	return (ptr);
 }
+
+/* 
+#include <stdio.h>
+
+void	print_list(t_list *lst)
+{
+	while (lst != NULL)
+	{
+		printf("%d ", *(int *)lst->content);
+		lst = lst->next;
+	}
+	printf("\n");
+}
+
+int	main(void)
+{
+	int		num;
+	int		num2;
+	int		num3;
+	t_list	*node;
+	t_list	*node2;
+	t_list	*node3;
+
+	num = 1986;
+	node = ft_lstnew(&num);
+	printf("Content of node1: %d\n", *(int *)node->content);
+	printf("Next pointer of node1: %p\n", (void *)node->next);
+
+	num2 = 10;
+	num3 = 20;
+	node2 = ft_lstnew(&num2);
+	node3 = ft_lstnew(&num3);
+	ft_lstadd_front(&node, node2);
+	ft_lstadd_back(&node, node3);
+
+	printf("Contents of the list:\n");
+	print_list(node);
+
+	while (node != NULL)
+	{
+		t_list *tmp = node->next;
+		free(node);
+		node = tmp;
+	}
+
+	return (0);
+} */
